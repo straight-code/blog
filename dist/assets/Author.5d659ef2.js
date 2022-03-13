7abe63497f9bc526a978eb179fa7809737a04189
straight-code <97550737+straight-code@users.noreply.github.com>
2022-03-13 01:14:42 +0000

    v1.3

---
+++ dist/assets/Author.5d659ef2.js
@@ -0,0 +1,8 @@
+import{c as d,a as h,b as m,r as p,d as i}from"./vendor.f28721e5.js";d("/src/components/Author.astro",{modules:[],hydratedComponents:[],hydrationDirectives:new Set([]),hoisted:[]});const l=h("/src/components/Author.astro","https://example.com/","file:///C:/Users/28sch/Documents/Desktop/straight-code-blog/blog/"),u=m(async(t,o,r)=>{const s=t.createAstro(l,o,r);s.self=u;const{name:a,href:e}=s.props,c=[{props:{"data-astro-id":"2Q6NI45U"},children:".author.astro-2Q6NI45U{margin-bottom:0.75rem;}"}];for(const n of c)t.styles.add(n);return p`
+
+<div class="author astro-2Q6NI45U">
+	<p class="astro-2Q6NI45U"><a${i(e,"href")} class="astro-2Q6NI45U">${a}</a></p>
+</div>
+
+
+`});
